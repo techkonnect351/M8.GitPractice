72f1031f57941f66788f1161520d981d07f9b073
91990 <91990@Shruvi_xiomi>
2024-01-24 10:39:25 +0530

    reflect changes

---
+++ src/test/java/Reflectchanges/changeclass.java
@@ -0,0 +1,12 @@
+package Reflectchanges;
+
+public class changeclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("testing");
+	}
+
+}
